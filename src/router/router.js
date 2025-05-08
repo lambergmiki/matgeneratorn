20e7585e463954d3b6d2b08d0ef2ab23ba1e0c01
Miki <ml227cr@student.lnu.se>
2025-05-08 15:44:31 +0200

    fix export issue of baseURL 2

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,12 @@
 import express from 'express'
 // import { router as homeRouter } from './homeRouter.js'
 import { router as recipeRouter } from './recipeRouter.js'
-import { baseUrl } from '../server.js'
+import { baseURL } from '../server.js'
 
 export const router = express.Router()
 
 // Define your routes here
-router.get(baseUrl, (req, res) => { res.render('home/index') })
+router.get(baseURL, (req, res) => { res.render('home/index') })
 
 // mount recipeRouter on /recipes
 router.use('/recipes', recipeRouter)
